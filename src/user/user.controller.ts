a101c295a83dd154bb6b911d3c05a25e175842f0
VitorPandini <valvespandini@gmail.com>
2024-03-21 17:33:06 -0300

    feat: insert typeorm in project

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userService.getAll;
   }
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return createUser;
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
   }
 }
